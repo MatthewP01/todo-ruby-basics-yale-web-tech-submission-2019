bb33a6335c7aa4a0817ca8d1189b3471f6def0e7
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-02-18 00:29:11 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,15 @@
 def division(num1, num2)
- 
+  div_num = num1 / num2
+  return div_num
 end
 
 def assign_variable(value)
-  
+  var = "#{value}"
+  return var
 end
 
 def argue
-  
+
 end
 
 def greeting
